3733f4b2c22f693351a524e0e5b83fa7bc3b23b0
Phurinut Waranich <phurinut.wara@gmail.com>
2025-05-20 17:07:51 +0700

    omz: add zsh-completions for command helper

--- omz/setup.zsh
+++ omz/setup.zsh
@@ -17,5 +17,6 @@ else
 	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+	git clone https://github.com/zsh-users/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
 	echo "DONE"
 fi

97e0f809d4b8c7b1c683ea570ddc44c1a7208878
Phurinut Waranich <phurinut.wara@gmail.com>
2025-11-24 11:10:33 +0700

    macos: add macos kb preferences info

--- macos/defaults.zsh
+++ macos/defaults.zsh
@@ -5,12 +5,23 @@
 # to reset all configurations use
 # `defaults delete -g` or `defaults delete NSGlobalDomain`
 
+# defaults delete -g KeyRepeat
+# defaults delete -g InitialKeyRepeat
+# บางกรณีต้องลบที่ currentHost ด้วย:
+# defaults -currentHost delete -g KeyRepeat
+# defaults -currentHost delete -g InitialKeyRepeat
+
+# to read configs
+# defaults read -g KeyRepeat
+# defaults read -g InitialKeyRepeat
+
 echo -n '[1/2] disabling ApplePressAndHoldEnabled...'
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 echo 'OK'
 
 echo -n '[2/2] adjusting InitialKeyRepeat and KeyRepeat...'
 # See https://apple.stackexchange.com/questions/10467/how-to-increase-keyboard-key-repeat-rate-on-os-x/83923#83923
+# Defaults are InitialKeyRepeat = ? , KeyRepeat = ?
 defaults write NSGlobalDomain InitialKeyRepeat -int 9
 defaults write NSGlobalDomain KeyRepeat -int 1
 echo 'OK'

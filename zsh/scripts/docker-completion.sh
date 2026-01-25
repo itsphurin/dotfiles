184b9f0e8e308539875a53c32305a289a76b770c
Phurinut Waranich <phurinut.wara@gmail.com>
2026-01-25 23:19:58 +0700

    zshrc: update .zshrc init for faster startup time

--- zsh/scripts/docker-completion.sh
+++
@@ -1,5 +0,0 @@
-# The following lines have been added by Docker Desktop to enable Docker CLI completions.
-fpath=(~/.docker/completions $fpath)
-autoload -Uz compinit
-compinit
-# End of Docker CLI completions

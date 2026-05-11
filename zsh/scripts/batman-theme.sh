e0169751becec07b1620a3b32d86433032d5146f
Phurinut Waranich <phurinut.wara@gmail.com>
2026-05-12 05:12:31 +0700

    claude: remove unnescessary plugins

--- zsh/scripts/batman-theme.sh
+++ zsh/scripts/batman-theme.sh
@@ -3,11 +3,7 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
   function man() {
     local theme
-    if [[ "$(defaults read -g AppleInterfaceStyle 2>/dev/null)" == "Dark" ]]; then
-      theme='Monokai Extended'
-    else
-      theme='Monokai Extended Light'
-    fi
+    # theme='Monokai Extended'
     eval "function man() { update_man_width; BAT_THEME='$theme' batman \"\$@\"; }"
     man "$@"
   }
